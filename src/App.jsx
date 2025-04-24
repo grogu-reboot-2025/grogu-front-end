2224008cbb4ea4d2eed8a00f33f5d3d7fa86dfaa
Luan Nguyen <luantcnguyen@gmail.com>
2025-04-24 09:47:01 +0100

    creates chat list page
    Signed-off-by: Luan Nguyen <luantcnguyen@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Demo } from "./pages/demo";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Topics } from "./pages/topics";
+import { ChatList } from "./pages/chatList";
 
 export const App = () => {
   return (
@@ -8,6 +9,7 @@ export const App = () => {
       <Routes>
         <Route path="/demo" element={<Demo />} />
         <Route path="/topics" element={<Topics />} />
+        <Route path="/chatlist" element={<ChatList />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/chatList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "../components/Card";
+import Container from "../components/Container";
+
+export const ChatList = () => {
+    return (
+<>
+<Card>
+  <Container></Container>
+</Card>
+</>
+    );
+  };
